2ee0a161a0a3194edc9bbf3d1415409190d71b65
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-10 19:46:12 +0530

    done

---
+++ src/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Index() {
+  return (
+    <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+      <hr class="my-4" />
+
+      <div class="pb-4">
+        <i class="fab fa-facebook-f mr-3"></i>
+
+        <i class="fab fa-twitter mr-3"></i>
+
+        <i class="fab fa-youtube mr-3"></i>
+
+        <i class="fab fa-google-plus-g mr-3"></i>
+
+        <i class="fab fa-whatsapp mr-3"></i>
+
+        <i class="fab fa-instagram mr-3"></i>
+      </div>
+    </footer>
+  );
+}
+
+export default Index;
